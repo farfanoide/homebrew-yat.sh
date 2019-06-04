b04763f6f3e6fc981c6c5e750ca4fb76fccafcc8
Ivan Karl <ivan6258@gmail.com>
2019-06-04 13:00:05 -0300

    updates release

--- yatsh.rb
+++ yatsh.rb
@@ -4,8 +4,8 @@ class Yatsh < Formula
   homepage "https://github.com/farfanoide/yat.sh"
   head "https://github.com/farfanoide/yat.sh.git"
 
-  url "https://github.com/farfanoide/yat.sh/archive/0.4.0.tar.gz"
-  sha256 "a80c1e53638b4470a6bbffbb03fa9de364bd9fcc4bb976e28e7d2fabd2f25267"
+  url "https://github.com/farfanoide/yat.sh/archive/0.4.1.tar.gz"
+  sha256 "5e81590a403222fabc7ef5db2d06186a287d7585a86720a7638789cc379a4644"
 
   depends_on "tmux" => :build
 
